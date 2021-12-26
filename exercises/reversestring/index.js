740a1da3e9ccb4382007e34ea94af50ddfe1c2f5
Michael <mkenton2020@icloud.com>
2021-12-26 04:05:46 -0600

    solved and passed tests on fizzbuzz and reversestrring exercises

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,24 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    
+    // // CLASSIC FOR LOOP
+    // let revStr = ""
+    // for (let i = str.length - 1; i >= 0; i--) {
+    //     revStr = revStr + str[i]
+    // }
+    // return revStr
+
+    // // USING NEWER SYNTAX
+    // for (let character of str) {
+    //     revStr = character + revStr
+    // }
+    // return revStr
+
+    // USING REDUCE HELPER
+    return str.split('').reduce((revStr, char) => char + revStr, '');
+    
+}
 
 module.exports = reverse;
