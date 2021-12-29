73020a75ce45bbaad5b79f9cafc1698c7fd02f40
Michael <mkenton2020@icloud.com>
2021-12-28 19:53:53 -0600

    solution for chunk exercise

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,27 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
-
+function chunk(array, size) { 
+    const arrLength = array.length
+    const lastChunkSize = arrLength % size
+    const numFullChunks = (arrLength - lastChunkSize) / size
+    
+    const result = []
+    let index = 0
+    for (let i = 0; i < numFullChunks; i++) {
+        let chunk = []
+        let j = 0
+        while (j < size) {
+            chunk[j] = array[index]
+            index = index + 1
+            j = j + 1
+        }
+        result[i] = chunk
+    }
+    if (lastChunkSize) {
+    const lastChunk = array.slice(-lastChunkSize);
+    result.push(lastChunk);
+    }
+    return result
+}
 module.exports = chunk;
